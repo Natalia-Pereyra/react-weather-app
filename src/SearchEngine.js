9ec47931d9b71e6a49ec9015e77a610177bf8e66
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-05-14 11:07:02 +1200

    Update SearchEngine.js

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 
 
 export default function SearchEngine(props) {
+  
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  
+  function handleResponse(response){
+     setTemperature(Math.round(response.data.main.temp));
+     setReady(true);
+   
+  }
 
-return (
+  if(ready) {
+    return (
 
   <div id="form">
 <form id="search-form">
@@ -13,4 +25,19 @@ return (
             </form>
             </div>
 );
+  } else {
+    
+  const apiKey = "96c00224132d4189b94cab19ab901e7";
+  let city = "London";
+  let url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(handleResponse);
+
+  return "Loading..."
+
+  }
+
+ 
+
+
+
 }
