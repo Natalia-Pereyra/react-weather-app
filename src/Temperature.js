a3b0c633881640e1ebcf66acb20b7ddcc06473ea
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-05-24 19:36:17 +1200

    Update Temperature.js

--- src/Temperature.js
+++ src/Temperature.js
@@ -23,7 +23,7 @@ return (
             <strong id="temperature">{props.data.temperature}</strong>
             <span className="units">
             <p id="celsius-link">°C</p>
-            <p id="fahrenheit-link">⎢<a href="#" onClick={showFahrenheit}>F</a></p>
+            <p id="fahrenheit-link">⎢<a href="/" onClick={showFahrenheit}>F</a></p>
             </span>
         </div>
         </div>
@@ -37,9 +37,9 @@ return (
         <div className="d-flex align-items-center">
        
         <div className="d-flex temperature"> 
-            <strong id="temperature">{fahrenheit}</strong>
+            <strong id="temperature">{Math.round(fahrenheit)}</strong>
             <span className="units">
-            <p id="celsius-link"><a href="#" onClick={showCelsius}>°C</a>⎢</p>
+            <p id="celsius-link"><a href="/" onClick={showCelsius}>°C</a>⎢</p>
             <p id="fahrenheit-link">F</p>
             </span>
         </div>
