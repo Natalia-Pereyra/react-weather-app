7526ba55a02fd7be61acb4bafb68d6cd02c45735
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-05-31 16:50:04 +1200

    Update Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer(){
         <div className="footer">
         <small id="my-name">
         <a id="my-link" href="https://github.com/Natalia-Pereyra/react-weather-app" target="_blank" rel="noreferrer">Open-source Code in GitHub </a>
-        by Natalia Pereyra and <a href="" target="_blank" rel="noreferrer">hosted on Netlify</a>
+        by Natalia Pereyra and <a href="https://gifted-knuth-a8eb5c.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
    </small>
    </div>
     )
