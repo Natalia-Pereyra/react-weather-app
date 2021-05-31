96af1c9b5d437d16d8e5a8077722ffb13f5bcfe1
Natalia-Pereyra <79956638+Natalia-Pereyra@users.noreply.github.com>
2021-05-31 17:02:10 +1200

    Added link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,8 @@ export default function Footer(){
     return(
         <div className="footer">
         <small id="my-name">
-        <a id="my-link" href="https://github.com/Natalia-Pereyra/react-weather-app" target="_blank" rel="noreferrer">Open-source Code in GitHub </a>
-        by Natalia Pereyra and <a href="https://gifted-knuth-a8eb5c.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify</a>
+        <a id="Github-link" href="https://github.com/Natalia-Pereyra/react-weather-app" target="_blank" rel="noreferrer">Open-source Code in GitHub </a>
+        by Natalia Pereyra and hosted on<a id="Netlify-link"href="https://gifted-knuth-a8eb5c.netlify.app/" target="_blank" rel="noreferrer"> Netlify</a>
    </small>
    </div>
     )
